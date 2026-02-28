b9b0991e2246f1254cb1ab9d19bfabe42a9d04e5
pratyush314 <pratyushshah314@gmail.com>
2026-02-28 22:27:49 +0530

    github and leetcode added in navbar, emailjs service fixed

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -30,8 +30,8 @@ const Contact = () => {
     try {
       console.log("From submitted:", formData);
       await emailjs.send(
-        "service_79b0nyj",
-        "template_17us8im",
+        "service_uv8vqyg",
+        "template_zhqrs09",
         {
           from_name: formData.name,
           to_name: "Pratyush",
@@ -39,7 +39,7 @@ const Contact = () => {
           to_email: "pratyushshah314@gmail.com",
           message: formData.message,
         },
-        "pn-Bw_mS1_QQdofuV",
+        "DuyvHRIYhJzQfwL6X",
       );
       setIsLoading(false);
       setFormData({ name: "", email: "", message: "" });
@@ -62,10 +62,11 @@ const Contact = () => {
       {showAlert && <Alert type={alertType} text={alertMessage} />}
       <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary">
         <div className="flex flex-col items-start w-full gap-5 mb-10">
-          <h2 className="text-heading">Let's Talk</h2>
+          <h2 className="text-heading">Let&apos;s Talk</h2>
           <p className="font-normal text-neutral-400">
-            Whether you're loking to build a new website, improve your existing
-            platform, or bring a unique project to life, I'm here to help
+            Whether you&apos;re loking to build a new website, improve your
+            existing platform, or bring a unique project to life, I&apos;m here
+            to help
           </p>
         </div>
         <form className="w-full" onSubmit={handleSubmit}>
